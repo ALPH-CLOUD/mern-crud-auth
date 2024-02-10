9a00ec54f7fd167e85b63e1068a29c58f92ed686
ALPH-CLOUD <71527311+ALPH-CLOUD@users.noreply.github.com>
2024-02-10 03:48:45 -0600

    login terminado

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,5 +1,44 @@
+import { InputForm } from "../components";
+import { useForm } from "react-hook-form";
+import { useAuth } from "../context/AuthContex";
+import { Link } from "react-router-dom";
+
 export const LoginPage = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const { signin, errors: LoginErrors } = useAuth();
+
+    const onSubmit = handleSubmit((data) => {
+        signin(data);
+    });
+
     return (
-        <div>LoginPage</div>
-    )
-}
+        <div className="flex h-[calc(100vh-100px)] items-center justify-center">
+            <div className="bg-zinc-800 max-w-md w-full p-10 rounded-md">
+                {LoginErrors.map((error, i) => (
+                    <div key={i} className="bg-red-500 p-2 mb-2 text-white text-center">
+                        {error}
+                    </div>
+                ))}
+                <h1 className="text-2xl font-bold">Login</h1>
+
+                <form onSubmit={onSubmit}>
+
+                    <InputForm typeInput="email" nameInput="email" handleInputState={register} placeholderInput="Email" />
+                    {errors.email && <p className="text-red-500">Email is required</p>}
+
+                    <InputForm typeInput="password" nameInput="password" handleInputState={register} placeholderInput="Password" />
+                    {errors.password && <p className="text-red-500">Password is required</p>}
+
+                    <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-1">
+                        Login
+                    </button>
+                </form>
+
+                <p className="flex gap-x-1 justify-between mt-2">
+                    Don't have an account? <Link to="/register" className="text-sky-500 cursor-pointer mr-3">Sign up</Link>
+                </p>
+
+            </div>
+        </div>
+    );
+};
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -3,51 +3,49 @@ import { InputForm } from '../components/index.js';
 import { useAuth } from '../context/AuthContex.jsx';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export const RegisterPage = () => {
-
-    const { register,
-        handleSubmit,
-        formState: {
-            errors
-        } } = useForm();
-
-    const { signup, isAuthenticated, errors:RegisterErrors} = useAuth();
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const { signup, isAuthenticated, errors: RegisterErrors } = useAuth();
     const navigate = useNavigate();
 
     useEffect(() => {
-        if (isAuthenticated) navigate('/tasks')
-    }, [isAuthenticated])
+        if (isAuthenticated) navigate('/tasks');
+    }, [isAuthenticated]);
 
     const onSubmit = handleSubmit(async (values) => {
         signup(values);
-    })
+    });
 
     return (
-        <div className='bg-zinc-800 max-w-md p-10 rounded-md'>
-            {
-                RegisterErrors.map((error, i) => (
-                    <div key = {i} className='bg-red-500 p-2 mb-2 text-white'>
+        <div className='flex h-screen items-center justify-center'>
+            <div className='bg-zinc-800 max-w-md w-full p-10 rounded-md'>
+                {RegisterErrors.map((error, i) => (
+                    <div key={i} className='bg-red-500 p-2 mb-2 text-white text-center'>
                         {error}
                     </div>
-                ))
-            }
-            <form
-                onSubmit={onSubmit}>
+                ))}
+                <h1 className='text-2xl font-bold mb-4'>Register</h1>
+                <form onSubmit={onSubmit}>
+                    <InputForm typeInput='text' nameInput='username' handleInputState={register} placeholderInput='Username' />
+                    {errors.username && <p className='text-red-500'>Username is required</p>}
 
-                <InputForm typeInput={"text"} nameInput={'username'} handleInputState={register} placeholderInput={"Username"} />
-                {errors.username && <p className='text-red-500'>Username is required</p>}
+                    <InputForm typeInput='email' nameInput='email' handleInputState={register} placeholderInput='Email' />
+                    {errors.email && <p className='text-red-500'>Email is required</p>}
 
-                <InputForm typeInput={"email"} nameInput={'email'} handleInputState={register} placeholderInput={"Email"} />
-                {errors.email && <p className='text-red-500'>Email is required</p>}
+                    <InputForm typeInput='password' nameInput='password' handleInputState={register} placeholderInput='Password' />
+                    {errors.password && <p className='text-red-500'>Password is required</p>}
 
-                <InputForm typeInput={"password"} nameInput={'password'} handleInputState={register} placeholderInput={"Password"} />
-                {errors.password && <p className='text-red-500'>Password is required</p>}
+                    <button type='submit' className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4'>
+                        Register
+                    </button>
+                </form>
 
-                <button type='submit'>
-                    Register
-                </button>
-            </form>
+                <p className='flex gap-x-2 justify-between mt-2'>
+                    Already have an account? <Link to='/login' className='text-sky-500 cursor-pointer mr-3'>Login</Link>
+                </p>
+            </div>
         </div>
-    )
+    );
 }
