c2397c0cf119fdd5d55a9332968510d095a8937b
ALPH-CLOUD <71527311+ALPH-CLOUD@users.noreply.github.com>
2024-02-10 02:00:29 -0600

    contexto de usuario y validación de errores terminado

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,24 +1,49 @@
 import { useForm } from 'react-hook-form';
 import { InputForm } from '../components/index.js';
-import { registerRequest } from '../api/auth.js';
+import { useAuth } from '../context/AuthContex.jsx';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 export const RegisterPage = () => {
 
-    const {register, handleSubmit } = useForm();
+    const { register,
+        handleSubmit,
+        formState: {
+            errors
+        } } = useForm();
 
-    const onSubmit = handleSubmit(async(values) => {
-        const res = await registerRequest(values)
-        console.log(res);
+    const { signup, isAuthenticated, errors:RegisterErrors} = useAuth();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (isAuthenticated) navigate('/tasks')
+    }, [isAuthenticated])
+
+    const onSubmit = handleSubmit(async (values) => {
+        signup(values);
     })
 
     return (
         <div className='bg-zinc-800 max-w-md p-10 rounded-md'>
-            <form 
+            {
+                RegisterErrors.map((error, i) => (
+                    <div key = {i} className='bg-red-500 p-2 mb-2 text-white'>
+                        {error}
+                    </div>
+                ))
+            }
+            <form
                 onSubmit={onSubmit}>
 
-                <InputForm typeInput={"text"} nameInput={'username'} handleInputState={register} placeholderInput={"Username"}/>
-                <InputForm typeInput={"email"} nameInput={'email'} handleInputState={register} placeholderInput={"Email"}/>
-                <InputForm typeInput={"password"} nameInput={'password'} handleInputState={register} placeholderInput={"Password"}/>
-                
+                <InputForm typeInput={"text"} nameInput={'username'} handleInputState={register} placeholderInput={"Username"} />
+                {errors.username && <p className='text-red-500'>Username is required</p>}
+
+                <InputForm typeInput={"email"} nameInput={'email'} handleInputState={register} placeholderInput={"Email"} />
+                {errors.email && <p className='text-red-500'>Email is required</p>}
+
+                <InputForm typeInput={"password"} nameInput={'password'} handleInputState={register} placeholderInput={"Password"} />
+                {errors.password && <p className='text-red-500'>Password is required</p>}
+
                 <button type='submit'>
                     Register
                 </button>
